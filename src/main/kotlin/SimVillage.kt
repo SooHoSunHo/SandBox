d13c12f024d2aa14fdefced380d1fad9c0d8bd80
SooHoSunHo <lssmyhero@gmail.com>
2022-07-07 16:34:55 +0900

    함수타입 반환

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,21 +1,21 @@
 fun main(args: Array<String>) {
 
-    runsimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("최석용"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "병원"
+    var numBuildings = 5
+    return {
+        playName: String ->
         val currentYear = 2022
-        println("$numBuildings 채의 건물이 추가됨")
-        "SimVillage 방문을 환영합니다. $playerName! (copyright $currentYear)"
+        numBuildings += 1
+        println("$numBuildings 채의 $structureType 이 추가됨")
+        "SimVillage 방문을 환영합니다. $playName! (copyright $currentYear)"
     }
-
-    //runsimulation("김선달", greetingFunction)
-}
-
-inline fun runsimulation(playerName: String, costPrinter:(Int) -> Unit, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..100).shuffled().last()
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-
-fun printConstructionCost(numBuildings: Int) {
-    val cost = 500
-    println("건축비용 : ${cost * numBuildings}")
 }
