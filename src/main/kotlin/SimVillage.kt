714e294eae6b8c08f199d2bf332d1e74cdc19fc9
SooHoSunHo <lssmyhero@gmail.com>
2022-07-07 16:18:49 +0900

    함수 참조 추가

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
 
-    runsimulation("김선달") { playerName, numBuildings ->
+    runsimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
         val currentYear = 2022
         println("$numBuildings 채의 건물이 추가됨")
         "SimVillage 방문을 환영합니다. $playerName! (copyright $currentYear)"
@@ -9,7 +9,13 @@ fun main(args: Array<String>) {
     //runsimulation("김선달", greetingFunction)
 }
 
-inline fun runsimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runsimulation(playerName: String, costPrinter:(Int) -> Unit, greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..100).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("건축비용 : ${cost * numBuildings}")
 }
